fc9f25730f6d2c42d44dc2264dc228deb63a0df2
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2023-05-06 19:46:14 +0200

    new templates from Zotero

--- app/index.ts
+++ app/index.ts
@@ -61,21 +61,30 @@ class ZoteroPlugin extends Generator {
       default: this.props.plugin.base.replace(prefix, '').replace(/(^|-)([a-z])/g, (g: string) => g.toUpperCase().replace(/-/g, ' ')).trim(),
     })
 
+    this.props.code.namespace = await this._private_ask({ message: 'Plugin Namespace', default: `Zotero.${this.props.plugin.name.replace(/ /g, '')}` })
+    this.props.code.namespace = `Zotero.${this.props.code.namespace.replace(/^Zotero./, '').replace(/[^a-z0-9]/gi, '')}`
+
     const template = {
-      'Overlay plugin for Zotero 6': 'src-1.0',
-      'Overlay plugin for Zotero 6 and bootstrapped plugin for Zotero 7': 'src-1.1',
+      // 'Overlay plugin for Zotero 6': 'src-1.0',
+      // 'Overlay plugin for Zotero 6 and bootstrapped plugin for Zotero 7': 'src-1.1',
       'Bootstrapped plugin for Zotero 6 and 7': 'src-1.2',
       'Bootstrapped plugin for Zotero 7': 'src-2.0',
     }
     this.props.code.template = await this._private_ask({ type: 'list', choices: Object.keys(template).sort(), message: 'What kind of extension are you building?' })
     this.props.code.template = template[this.props.code.template]
+  }
 
-    this.props.code.namespace = await this._private_ask({ message: 'Plugin Namespace', default: `Zotero.${this.props.plugin.name.replace(/ /g, '')}` })
-    this.props.code.namespace = `Zotero.${this.props.code.namespace.replace(/^Zotero./, '').replace(/[^a-z0-9]/gi, '')}`
+  private _private_move(source: string, target: string): void {
+    const dest_source = this.destinationPath(source)
+
+    if (this.fs.exists(dest_source)) {
+      this.fs.move(dest_source, this.destinationPath(target))
+    }
   }
 
   public writing(): void {
     const base = 'client'
+    const root = path.join(base, '..')
 
     this.fs.copy(
       this.templatePath(path.join('make-it-red', this.props.code.template, '**')),
@@ -90,24 +99,33 @@ class ZoteroPlugin extends Generator {
           source = source.replace(/[.]make-it-red[.]/g, `.${this.props.plugin.base}.`)
           source = source.replace(/[/]make-it-red[/]/g, `/${this.props.plugin.base}/`)
           source = source.replace(/Make It Red/g, `/${this.props.plugin.name}/`)
-          source = source.replace(/Makes everything red/g, `/${this.props.plugin.name}/`)
-          source = source.replace(/Zotero[.]MakeItRed/g, `/Zotero.${this.props.code.namespace}/`)
-          source = source.replace(/make-it-red([-.])/g, `/${this.props.plugin.base}$1`)
+          source = source.replace(/Makes everything red/g, `${this.props.plugin.name}`)
+          source = source.replace(/Zotero[.]MakeItRed/g, this.props.code.namespace)
+          source = source.replace(/make-it-red([-.])/g, `${this.props.plugin.base}$1`)
           source = source.replace(/locale\s+make-it-red\s+en-US/, `locale ${this.props.plugin.base} en-US`)
+
+          // source = source.replace(/;$/gm, '').replace(/\t/, '  ').replace(/\s+$/gm, '')
+
           if (source !== contents.toString('utf-8')) return source
           return contents
         },
       }
     )
-    this.fs.move(
-      this.destinationPath(path.join(base, 'locale', 'en-US', 'make-it-red.ftl')),
-      this.destinationPath(path.join(base, 'locale', 'en-US', `${this.props.plugin.base}.ftl`))
+
+    this._private_move(
+      path.join(base, 'locale', 'en-US', 'make-it-red.ftl'),
+      path.join(base, 'locale', 'en-US', `${this.props.plugin.base}.ftl`)
     )
-    this.fs.move(
-      this.destinationPath(path.join(base, 'chrome', 'locale', 'en-US', 'make-it-red.properties')),
-      this.destinationPath(path.join(base, 'chrome', 'locale', 'en-US', `${this.props.plugin.base}.properties`))
+    this._private_move(
+      path.join(base, 'chrome', 'locale', 'en-US', 'make-it-red.properties'),
+      path.join(base, 'chrome', 'locale', 'en-US', `${this.props.plugin.base}.properties`)
     )
-    this.fs.delete(this.destinationPath(path.join(base, 'install.rdf')))
+    this._private_move(path.join(base, 'bootstrap.js'), path.join(root, 'bootstrap.ts'))
+    this._private_move(path.join(base, 'lib.js'), path.join(root, 'lib.ts'))
+    this._private_move(path.join(base, 'esbuild.js'), path.join(root, 'esbuild.js'))
+
+    this.fs.copy(path.join(__dirname, 'tsconfig.json'), this.destinationPath(path.join(root, 'tsconfig.json')))
+    this.fs.copy(path.join(__dirname, 'eslintrc.json'), this.destinationPath(path.join(root, '.eslintrc.json')))
 
     const version = '0.0.1'
     this.fs.writeJSON(this.destinationPath('package.json'), {
@@ -142,6 +160,9 @@ class ZoteroPlugin extends Generator {
       },
     })
 
+    // overwrite during build bij zotero-plugin
+    // this.fs.delete(this.destinationPath(path.join(base, 'install.rdf')))
+
     if (this.fs.exists(this.destinationPath(path.join(base, 'manifest.json')))) {
       this.fs.writeJSON(this.destinationPath(path.join(base, 'manifest.json')), {
         manifest_version: 2,
@@ -162,7 +183,8 @@ class ZoteroPlugin extends Generator {
     this.env.options.nodePackageManager = 'npm'
   }
 
-  public end() {
+  public end(): void {
+    this.spawnCommandSync('npm', ['run', 'lint', '--', '--fix'])
     this.log('also look at the `zotero-plugin` package that is now installed for your plugin')
   }
 }
